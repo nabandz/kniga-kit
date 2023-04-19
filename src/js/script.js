91bdaf84a10d8b64483c954b0e9e2e6ae12f6606
Natasha Bandzeladze <na.bandzeladze@gmail.com>
2023-04-20 02:31:33 +0300

    feature: add adaptation for sections playtest, footer

--- src/js/script.js
+++ src/js/script.js
@@ -28,7 +28,6 @@ window.addEventListener('DOMContentLoaded', function() {
 
     //Slider - section Approve
     const swiperApprove = new Swiper(".approve__slider", {
-        autoHeight: true,
         loop: true,
         mousewheel: true,
         spaceBetween: 50,
