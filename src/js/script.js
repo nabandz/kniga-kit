f653aea6c5d501afe1d99f15cc9d6ebe4cdac773
Natasha Bandzeladze <na.bandzeladze@gmail.com>
2023-04-19 17:29:30 +0300

    feature: add adaptation for section promo

--- src/js/script.js
+++ src/js/script.js
@@ -1,4 +1,20 @@
 window.addEventListener('DOMContentLoaded', function() {
+
+    //Menu
+    const mobileMenu = document.querySelector('.header__nav'),
+          menuLinks = document.querySelectorAll('.header__link'),
+          menuButton = document.querySelector('.header__menu-button');
+
+    menuButton.addEventListener('click', () => {
+        mobileMenu.classList.toggle('header__nav_active');
+    });
+
+    menuLinks.forEach(link => {
+        link.addEventListener('click', () => {
+            mobileMenu.classList.toggle('header__nav_active');
+        });
+    });
+
     //Slider - section Stories
     const swiperStories = new Swiper(".stories__slider", {
         loop: true,
@@ -9,6 +25,7 @@ window.addEventListener('DOMContentLoaded', function() {
             delay: 3000,
         },
     });
+
     //Slider - section Approve
     const swiperApprove = new Swiper(".approve__slider", {
         loop: true,
