7714b873413975014cea8ca102a785b1602a8274
Natasha Bandzeladze <na.bandzeladze@gmail.com>
2023-04-19 15:03:54 +0300

    feature: add slider for section stories

--- src/js/script.js
+++ src/js/script.js
@@ -1,6 +1,16 @@
 window.addEventListener('DOMContentLoaded', function() {
-    //Slider
-    const swiper = new Swiper(".approve__slider", {
+    //Slider - section Stories
+    const swiperStories = new Swiper(".stories__slider", {
+        loop: true,
+        effect: "fade",
+        spaceBetween: 50,
+        speed: 2500,
+        autoplay: {
+            delay: 3000,
+        },
+    });
+    //Slider - section Approve
+    const swiperApprove = new Swiper(".approve__slider", {
         loop: true,
         mousewheel: true,
         spaceBetween: 50,
