336691a3491f9aa46a87e15a15e737d6fceb36ec
Natasha Bandzeladze <na.bandzeladze@gmail.com>
2023-07-08 21:32:02 +0300

    fix: update header, fix before/after elements

--- src/js/app.js
+++ src/js/app.js
@@ -1,7 +1,9 @@
 import * as flsFunctions from "./modules/functions.js";
 import * as silderFunction from "./modules/slider.js";
 import * as menuFunction from "./modules/menu.js"
+import * as headerFunction from "./modules/header.js"
 
 flsFunctions.isWebp();
 silderFunction.slider();
 menuFunction.menu();
+headerFunction.headerActive();
---
+++ src/js/modules/header.js
@@ -0,0 +1,12 @@
+export function headerActive() {
+    
+    const header = document.querySelector('.header');
+
+    window.addEventListener('scroll', function() {
+        if (window.scrollY > 0) {
+            header.classList.add('header_active');
+        } else {
+            header.classList.remove('header_active');
+        }
+    });
+}
