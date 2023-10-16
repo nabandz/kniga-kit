c501ba2aebe2bb91ab82b6cfebf5a2acb3adb41b
Natasha Bandzeladze <na.bandzeladze@gmail.com>
2023-10-17 02:58:34 +0300

    feature: redesign section stories

---
+++ src/js/modules/tabcontent.js
@@ -0,0 +1,29 @@
+const tabs = document.querySelectorAll(".tabcontent__item"),
+  tabsContent = document.querySelectorAll(".tabcontent__picture");
+
+function hideTabContent() {
+  tabsContent.forEach((item) => {
+    item.classList.add("hide");
+    item.classList.remove("show", "fade");
+  });
+
+  tabs.forEach((item) => {
+    item.classList.remove("tabcontent__item_active");
+  });
+}
+
+function showTabContent(i = 0) {
+  tabsContent[i].classList.add("show", "fade");
+  tabsContent[i].classList.remove("hide");
+  tabs[i].classList.add("tabcontent__item_active");
+}
+
+hideTabContent();
+showTabContent();
+
+tabs.forEach((element, i) => {
+  element.addEventListener("click", function () {
+    hideTabContent();
+    showTabContent(i);
+  });
+});
